406b36f8203aed8db94fa350e17f773fe083cbc1
blar khalid <blarharki@gmail.com>
2018-09-05 07:28:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ return  aray.map(function(drivers){
  return drivers.toLowerCase();
 })
 }
-function nameToAttributes(){}
+function nameToAttributes (aray) {
+  return aray.map(function (drivers) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
